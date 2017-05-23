8fa7d22a07a75e661133bf74e3fbd62ea037be28
paltapas3 <paltapas3@gmail.com>
2017-05-23 09:54:47 +0530

    Cognitive computer vision API sample
    C# console application will demonstrate how to retrieve image features
    in JSON format like Image properties, tags & description from a selected
    image using cognitive service computer vision API.

---
+++ ComputerVisionAPI.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Configuration;
+
+namespace ComputerVisionAPI
+{
+    class ComputerVisionAPI
+    {
+        
+        static string Subscriptionkey()
+        {
+            return System.Configuration.ConfigurationManager.AppSettings["Subscription-Key"];
+            
+        }
+        static string RequestParameters()
+        {
+            return System.Configuration.ConfigurationManager.AppSettings["RequestParameters"];
+
+        }
+        static string ReadImagePath()
+        {
+            return System.Configuration.ConfigurationManager.AppSettings["ImagePath"];
+
+        }
+        static string ReadURI()
+        {
+            return System.Configuration.ConfigurationManager.AppSettings["APIuri"];
+
+        }
+        static string Contenttypes()
+        {
+            return System.Configuration.ConfigurationManager.AppSettings["Contenttypes"];
+
+        }
+
+        static void Main(string[] args)
+        {
+            GetImgeDetails(ReadImagePath());
+          
+        }
+       
+        static byte[] GetImageAsByteArray(string ImagePath)
+        {
+            FileStream ImagefileStream = new FileStream(ImagePath, FileMode.Open, FileAccess.Read);
+            BinaryReader ImagebinaryReader = new BinaryReader(ImagefileStream);
+            return ImagebinaryReader.ReadBytes((int)ImagefileStream.Length);
+        }
+        /// <summary>
+        /// Following function to fetch all image related details
+        /// </summary>
+        /// <param name="ImagePath"></param>
+        static  void GetImgeDetails(string ImagePath)
+        {
+            var ComputerVisionAPIclient = new HttpClient();
+
+            // Request headers - replace this example key with your valid subscription key. I have added that in App.config
+            ComputerVisionAPIclient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", Subscriptionkey());
+
+            // Request parameters.
+            string requestParameters = RequestParameters();
+            string APIuri = ReadURI() + requestParameters;
+          
+            // Request body. 
+            byte[] ImagebyteData = GetImageAsByteArray(ImagePath);
+
+            ImgeAnalysis(ImagebyteData, APIuri, ComputerVisionAPIclient);
+
+            
+        }
+        /// <summary>
+        /// Following function calls the computer vision API & display the response in Console
+        /// </summary>
+        /// <param name="ImagebyteData"></param>
+        /// <param name="uri"></param>
+        /// <param name="ComputerVisionAPIclient"></param>
+        static async void ImgeAnalysis(byte[] ImagebyteData, string APIuri, HttpClient ComputerVisionAPIclient)
+
+        {
+            HttpResponseMessage APIresponse;
+            var Imagecontent = new ByteArrayContent(ImagebyteData);
+            Imagecontent.Headers.ContentType = new MediaTypeHeaderValue(Contenttypes());
+            APIresponse = await ComputerVisionAPIclient.PostAsync(APIuri, Imagecontent);
+            Console.WriteLine(APIresponse);
+            Console.Read();
+            
+
+        }
+
+    }
+}
